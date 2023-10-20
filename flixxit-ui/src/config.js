353195af0b274d22c26506b0bdd54d692fb2935e
afifakanchwala <121440666+afifakanchwala@users.noreply.github.com>
2023-10-20 12:51:50 +0530

    Changes done

--- flixxit-ui/src/config.js
+++ flixxit-ui/src/config.js
@@ -1,4 +1,5 @@
 export const API_KEY = "f4e3b09446ad09d33155258c89aabf68";
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
-// export const BASE_MONGO_URL = "https://backend-flixxit.onrender.com";
-export const BASE_MONGO_URL = "https://localhost:5000";
+
+// export const BASE_MONGO_URL = "https://localhost:5000";
+export const BASE_MONGO_URL = "https://flixxit-backend-12uj.onrender.com";
